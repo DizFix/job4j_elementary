ce33398018d0a5c76498c752f991097348279722
DizFix <my_name_is_ilya@mail.ru>
2020-07-24 14:22:43 +0300

    4.5. Отладка программы в IDEA[159464#292731]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -3,8 +3,7 @@ package ru.job4j.condition;
 public class MultiMax {
     public static int max(int first, int second, int third) {
         int result = first > second ? first : second;
-        int result1 = result > third ? result : third;
-        return result1;
+        return result > third ? result : third;
     }
     public static void main(String[] args) {
         int msg = MultiMax.max(17, 16, 16);
