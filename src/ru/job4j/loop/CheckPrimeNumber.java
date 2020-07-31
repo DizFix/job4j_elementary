cbf43818687a4754cb3cf95a01e26c6eb15dd639
DizFix <my_name_is_ilya@mail.ru>
2020-07-31 21:58:16 +0300

    5.5. Простое число[171694#292821]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,6 +4,7 @@ public class CheckPrimeNumber {
     public static void main(String[] args) {
         check(6);
         check(5);
+        check(1);
     }
 
     public static boolean check(int number) {
@@ -12,8 +13,13 @@ public class CheckPrimeNumber {
             if (number % count == 0) {
                 prime = false;
                 break;
-                return prime;
             }
         }
+        if (prime == true) {
+            System.out.println(number + " " + "является простым числом");
+        } else {
+            System.out.println(number + " " + "не является простым числом");
+        }
+        return prime;
     }
 }
