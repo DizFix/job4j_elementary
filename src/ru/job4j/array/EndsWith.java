b0978d80f92e4fc9deed39f6a4036d2548d6f661
DizFix <my_name_is_ilya@mail.ru>
2020-10-18 10:39:13 +0300

    6.4.1. Слово заканчивается на ...[156317#292748]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,18 +3,8 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int index = 0; index < word.length / 2; index++) {
-            char temp = word[index];
-            word[index] = word[word.length - index - 1];
-            word[word.length - index - 1] = temp;
-        }
-        for (int index = 0; index < post.length / 2; index++) {
-            char temp = post[index];
-            post[index] = post[post.length - index - 1];
-            post[post.length - index - 1] = temp;
-        }
         for (int index = 0; index < post.length; index++) {
-            if (post[index] != word[index]) {
+            if (post[post.length - index - 1] != word[word.length - index - 1]) {
                 result = false;
                 break;
                 /* проверить. что массив word имеет последние элементы одинаковые с post */
