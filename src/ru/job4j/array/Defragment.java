bb6a897ad3476897f2b1d9e1739bcbd172aca3ef
DizFix <my_name_is_ilya@mail.ru>
2021-03-21 10:37:49 +0300

    6.8. Дефрагментация массива.[225#292763]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -4,12 +4,15 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int nullIndex = index; /* указатель на null ячейку. */
-                /* переместить первую не null ячейку. Нужен цикл. */
-            }
-            for (int index = 0; index < array.length; index++) {
-                if (array[index] != null) {
-                    int notNullInde = index;
+                int nullIndex = index;
+                for (int index2 = index + 1; index2 < array.length; index2++) {
+                    if (array[index2] != null) {
+                        int notnullIndex = index2;
+                        String temp = array[nullIndex];
+                        array[nullIndex] = array[notnullIndex];
+                        array[notnullIndex] = temp;
+                        break;
+                    }
                 }
             }
             System.out.print(array[index] + " ");
